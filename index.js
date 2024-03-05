b167706de17c29a01990284a509fa3a054c86ed3
SivasankarH <155282396+SivasankarH@users.noreply.github.com>
2024-03-05 10:46:48 +0530

    Used forEach to properly loop through the divs so the eventListener can work

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 //First step is to make 16x16 divs through for loop
-    const container = document.querySelector('#container');
-    for(i=0; i<255; i++){
-       const divs = document.createElement('div');
-       divs.classList.add('divs');  
-        container.appendChild(divs)
-    }
+const container = document.querySelector("#container");
+for (i = 0; i < 255; i++) {
+  const divs = document.createElement("div");
+  divs.classList.add(`divs${i}`, "divs");
+  container.appendChild(divs);
+}
 
+const divList = document.querySelectorAll(".divs");
 
-
+divList.forEach((divs) => {
+ 
+divs.addEventListener("mouseenter", () => {
+  console.log("yay");
+  divs.classList.add("black");
+}); });
