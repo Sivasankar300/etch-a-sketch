130511f5b903cc340cda59b07e1996e264aa2a16
SivasankarH <155282396+SivasankarH@users.noreply.github.com>
2024-03-07 20:46:39 +0530

    Added comments and cleaned up the code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//First step is to make 16x16 divs through for loop
+// Creating 16x16 squares of div when first entering the site
 const container = document.querySelector("#container");
 for (i = 0; i < 255; i++) {
   const divs = document.createElement("div");
@@ -6,21 +6,51 @@ for (i = 0; i < 255; i++) {
   container.appendChild(divs);
 }
 
-const divList = document.querySelectorAll(".divs");
-const button = document.querySelector('#btn');
-
-divList.forEach((divs) => {
- 
-divs.addEventListener("mouseenter", () => {
-  console.log("yay");
-  divs.classList.add("black");
-}); });
-
-const divs = document.querySelector('divs');
+const button = document.querySelector("#btn");
 
 
-button.addEventListener('click', () => {
-    while(container.hasChildNodes()) {
-     const deletedDivs = container.removeChild(container.firstChild);
+// Adding the "black" class to a div when being hovered by a mouse
+
+function hoverEffect() {
+  const divList = document.querySelectorAll(".divs");
+  divList.forEach((divs) => {
+    divs.addEventListener("mouseenter", () => {
+      console.log("yay");
+      divs.classList.add("black");
+    });
+  });
+}
+
+// Button that prompts the user for number of squares of per side for the new grid and replace the old one in the same amount of space
+
+button.addEventListener("click", () => {
+  let userInput = prompt("How many divs per side?");
+
+
+// Making sure that the user can't input above 100 per side to avoid the browser from crashing or lagging hard 
+
+  if (userInput)
+    if (userInput <= 100) {
+      while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+      }
+
+      userInput *= userInput;
+
+      for (i = 0; i < userInput; i++) {
+        const divs = document.createElement("div");
+        divs.classList.add(`divs${i}`, "divs");
+        
+
+
+
+        container.appendChild(divs);
+      }
+
+
+      const divList = document.querySelectorAll(".divs");
+      hoverEffect();
+    } else {
+      alert("Too many, Refresh and keep it under 100");
     }
 });
