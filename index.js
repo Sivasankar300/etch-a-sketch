c37aa14a78680ba35c3e50cfbff9d11fc0366fef
SivasankarH <155282396+SivasankarH@users.noreply.github.com>
2024-03-07 22:16:00 +0530

    Fixed an Issue with overflowing squares and cleaned up the code
    The squares would overflow at higher amounts of them which was fixed by removing "min height".

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Creating 16x16 squares of div when first entering the site
 const container = document.querySelector("#container");
-for (i = 0; i < 255; i++) {
+for (let i = 0; i < 255; i++) {
   const divs = document.createElement("div");
   divs.classList.add(`divs${i}`, "divs");
   container.appendChild(divs);
@@ -8,14 +8,12 @@ for (i = 0; i < 255; i++) {
 
 const button = document.querySelector("#btn");
 
-
 // Adding the "black" class to a div when being hovered by a mouse
 
 function hoverEffect() {
   const divList = document.querySelectorAll(".divs");
   divList.forEach((divs) => {
     divs.addEventListener("mouseenter", () => {
-      console.log("yay");
       divs.classList.add("black");
     });
   });
@@ -26,8 +24,7 @@ function hoverEffect() {
 button.addEventListener("click", () => {
   let userInput = prompt("How many divs per side?");
 
-
-// Making sure that the user can't input above 100 per side to avoid the browser from crashing or lagging hard 
+  // Making sure that the user can't input above 100 per side to avoid the browser from crashing or lagging hard
 
   if (userInput)
     if (userInput <= 100) {
@@ -40,13 +37,9 @@ button.addEventListener("click", () => {
       for (i = 0; i < userInput; i++) {
         const divs = document.createElement("div");
         divs.classList.add(`divs${i}`, "divs");
-        
-
-
 
         container.appendChild(divs);
       }
-
 
       const divList = document.querySelectorAll(".divs");
       hoverEffect();
@@ -54,3 +47,4 @@ button.addEventListener("click", () => {
       alert("Too many, Refresh and keep it under 100");
     }
 });
+hoverEffect();
